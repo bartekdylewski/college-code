1d4760d330d1800d003ce9b30f0cbb4dbcff6026
bartekdylewski <dylewskipoczta@gmail.com>
2024-10-18 10:55:45 +0200

    reorg file system

--- 1s/ps/lab3/cmake-build-debug/cmake_install.cmake
+++ 1s/ps/lab3/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/bartek/Documents/GitHub/college-projects/1s/lab3
+# Install script for directory: /home/bartek/Documents/GitHub/college-projects/1s/ps/lab3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/bartek/Documents/GitHub/college-projects/1s/lab3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/bartek/Documents/GitHub/college-projects/1s/ps/lab3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
